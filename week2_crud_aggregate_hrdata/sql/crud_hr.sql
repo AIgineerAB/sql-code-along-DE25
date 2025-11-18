a31f2e54772d041c1f725d1f201fb258da831a01
Db-Lau <debbielau.hk@gmail.com>
2025-11-18 13:26:18 +0100

    update crud codes

--- week2_crud_aggregate_hrdata/sql/crud_hr.sql
+++ week2_crud_aggregate_hrdata/sql/crud_hr.sql
@@ -46,3 +46,28 @@ UPDATE staging.employees
 SET employment_year = 2023
 WHERE employee_id IN (98,99);
 --WHERE employee_id = 98 OR employee_id = 99; 
+
+/* ============
+   DDL - Alter
+   ============ */
+ALTER TABLE staging.employees
+ADD COLUMN pension_plan VARCHAR DEFAULT 'plan 1'
+
+/* ==============
+   CRUD - Update 
+   ============== */
+UPDATE staging.employees
+SET pension_plan = 'plan 2'
+WHERE employment_year > 2015; 
+
+/* ==============
+   CRUD - Delete 
+   ============== */
+-- always check the rows you plan to delete first
+SELECT *
+FROM staging.employees
+WHERE employee_id = 1; 
+
+DELETE
+FROM staging.employees
+WHERE employee_id = 1; 
