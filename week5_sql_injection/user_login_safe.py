b7c0ea7d02900f49d38e974b9848ac34602e56dd
kokchun <kokchungiang@gmail.com>
2025-12-10 13:13:47 +0100

    protected against sql injection

---
+++ week5_sql_injection/user_login_safe.py
@@ -0,0 +1,23 @@
+import duckdb
+from constants import DB_PATH
+
+username = input("Enter username: ")
+password = input("Enter password: ")
+
+with duckdb.connect(str(DB_PATH)) as conn:
+    # protects against SQL injection
+    result = conn.execute(
+        "SELECT * FROM users WHERE username = ? AND password = ?",
+        parameters=(username, password),
+    )
+
+    if result.fetchall():
+        print("You can eat all ice creams you want")
+    else:
+        print("Sorry dude")
+
+    # a ' OR '1'='1' --
+    # WHERE username = 'a ' OR '1'='1' -- AND password = ...
+    # False or True -> True
+
+    # ';DROP TABLE users;--
