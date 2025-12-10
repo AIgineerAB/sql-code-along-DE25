0d609a44644c538f32bfe39024efe81c4c65ca58
kokchun <kokchungiang@gmail.com>
2025-12-10 13:09:10 +0100

    unsafe done

--- week5_sql_injection/user_login_unsafe.py
+++ week5_sql_injection/user_login_unsafe.py
@@ -5,14 +5,7 @@ username = input("Enter username: ")
 password = input("Enter password: ")
 
 with duckdb.connect(str(DB_PATH)) as conn:
-    result = conn.execute(f"""
-        SELECT 
-            *
-        FROM 
-            users
-        WHERE
-            username = '{username}' AND
-            password = '{password}'
+    result = conn.execute(f"""SELECT * FROM users WHERE username = '{username}' AND password = '{password}'
     """)
     # if the password is correct 
     # we will get back a record in the list -> truthy
@@ -23,3 +16,9 @@ with duckdb.connect(str(DB_PATH)) as conn:
         print("You can eat all ice creams you want")
     else:
         print("Sorry dude")
+
+    # a ' OR '1'='1' --
+    # WHERE username = 'a ' OR '1'='1' -- AND password = ...
+    # False or True -> True
+
+    # ';DROP TABLE users;--
